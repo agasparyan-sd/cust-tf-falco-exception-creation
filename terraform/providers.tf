189d66294637d7bd35f75081f55a9ede1251ee6b
Aram Gasparyan <aram.gasparyan@sysdig.com>
2023-09-01 15:05:13 -0700

    removed sysdig secure tokan variable

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,3 @@
 provider "sysdig" {
-  sysdig_secure_url="https://us2.app.sysdig.com"
-  sysdig_secure_api_token = "${var.sysdig_secure_token}"
+
 }
--- terraform/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "sysdig_secure_token" {
- type = string
- default = "<token>" # any value that is new
- description = "Sysdig API Token"
-}
